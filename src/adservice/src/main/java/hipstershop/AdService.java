24181088d4d3ecb2fc857db63de3287e8fab3a21
mofayad96 <cds.mohamedsalah30408@alexu.edu.eg>
2026-01-10 00:57:32 +0200

    sonarqube ci cd

--- src/adservice/src/main/java/hipstershop/AdService.java
+++ src/adservice/src/main/java/hipstershop/AdService.java
@@ -228,7 +228,7 @@ public final class AdService {
               initTracing();
             })
         .start();
-
+//comment
     // Start the RPC server. You shouldn't see any output from gRPC before this.
     logger.info("AdService starting.");
     final AdService service = AdService.getInstance();
