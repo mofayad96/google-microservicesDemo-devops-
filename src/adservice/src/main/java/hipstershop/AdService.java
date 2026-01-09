200fe28b0b8a878d2da842047cc0465b5d4b84bc
mofayad96 <cds.mohamedsalah30408@alexu.edu.eg>
2026-01-10 01:41:51 +0200

    editing some sonarqube configs, properties

--- src/adservice/src/main/java/hipstershop/AdService.java
+++ src/adservice/src/main/java/hipstershop/AdService.java
@@ -230,6 +230,7 @@ public final class AdService {
         .start();
 //commentaafdajf
 //asknsfa
+//akdsnka
     // Start the RPC server. You shouldn't see any output from gRPC before this.
     logger.info("AdService starting.");
     final AdService service = AdService.getInstance();
