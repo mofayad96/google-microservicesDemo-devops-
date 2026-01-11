8e821748d07acb1e25a8fc2f78ee5c959856253b
mofayad96 <cds.mohamedsalah30408@alexu.edu.eg>
2026-01-11 04:37:24 +0200

    testing trivy in workflow

--- src/adservice/src/main/java/hipstershop/AdService.java
+++ src/adservice/src/main/java/hipstershop/AdService.java
@@ -229,7 +229,7 @@ public final class AdService {
             })
         .start();
 
-//
+//ads
     // Start the RPC server. You shouldn't see any output from gRPC before this.
     logger.info("AdService starting.");
     final AdService service = AdService.getInstance();
