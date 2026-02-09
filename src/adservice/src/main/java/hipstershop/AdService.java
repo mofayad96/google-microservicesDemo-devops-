ecdf6676479460d421badb7d94de5ed12b7d3a01
mofayad96 <cds.mohamedsalah30408@alexu.edu.eg>
2026-02-09 09:25:48 +0200

    test

--- src/adservice/src/main/java/hipstershop/AdService.java
+++ src/adservice/src/main/java/hipstershop/AdService.java
@@ -228,7 +228,7 @@ public final class AdService {
               initTracing();
             })
         .start();
-
+//asd
     // Start the RPC server. You shouldn't see any output from gRPC before this.
     logger.info("AdService starting.");
     final AdService service = AdService.getInstance();
